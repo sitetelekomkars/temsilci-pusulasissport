cfc8469978f15cd6933d9d54e34eaec26b728d46
dogus <karssitetelekom@gmail.com>
2026-04-11 18:26:29 +0300

    Update app.js

--- jss/app.js
+++ jss/app.js
@@ -50,7 +50,7 @@ function formatShiftDate(d) {
     return d;
 }
 
-const BAKIM_MODU = false;
+const BAKIM_MODU = true;
 
 function showGlobalError(message) {
     // Kullanıcılara kırmızı bant gösterme (istek: ekran temiz kalsın)
